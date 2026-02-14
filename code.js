b668b1a69782f84b883448d36cfd18958daee6eb
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-02-13 23:49:09 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,4 +2,9 @@ const Name = "Sam";
 
 const Age = 17;
 
-console.log ('Hello i am' + name + 'And i am' + Age + 'I idk know what else to put here TwT');
+console.log ('Hello i am' + Name + 'And i am' + Age + 'I idk know what else to put here TwT');
+
+if(age >=18){console,log("welcome to this site");}
+else {
+    console.log("you must be 18+");
+}
