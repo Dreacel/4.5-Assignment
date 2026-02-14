dfba825dc716532dc522c6348ade6049302c0358
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-02-13 23:13:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-const name = Sam;
+const Name = Sam;
 
 const Age = 17;
 
-console.log ("Hello my name is" + name + "And i am " + Age);
+console.log ("Hello my name is" + name + "And i am " + age + "I don't know what else to put here TwT");
